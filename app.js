7976d4483c98f3949c7bda65b9af3c8118e25a8f
plkujaw <jawor.jakub@gmail.com>
2020-06-06 13:30:13 +0100

    added random background colour on page load

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const colours = ["#be5046", "#98c379", "#e5c07b", "#61afef", "#c578dd", "#56b5c2"];
+const colours = ["#abb2bf", "#be5046", "#98c379", "#e5c07b", "#61afef", "#c578dd", "#56b5c2"];
 const btn = document.getElementById("btn");
 const colour = document.querySelector(".colour")
 
-btn.addEventListener("click", function() {
-  // get random number between 0 and colours array length
-  const randomNumber = getRandomNumber();
-console.log(randomNumber);
 
-  document.body.style.backgroundColor = colours[randomNumber];
-  colour.textContent = colours[randomNumber];
-  colour.style.color = colours[randomNumber];
+btn.addEventListener("click", function() {
+  setRandomBackground();
 });
 
 function getRandomNumber() {
-
+  // get random number between 0 and colours array length
   return Math.floor(Math.random() * colours.length);
+}
+
+function setRandomBackground() {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colours[randomNumber];
+  colour.textContent = colours[randomNumber];
+  colour.style.color = colours[randomNumber];
 }
