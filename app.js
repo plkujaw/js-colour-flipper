b99fa1a3fd78ad56403f98801c833be776120b5c
plkujaw <jawor.jakub@gmail.com>
2020-06-06 14:20:11 +0100

    updated styles

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colours = ["#abb2bf", "#be5046", "#98c379", "#e5c07b", "#61afef", "#c578dd", "#56b5c2"];
+const colours = ["#be5046", "#98c379", "#e5c07b", "#61afef", "#c578dd", "#56b5c2"];
 const btn = document.getElementById("btn");
 const colour = document.querySelector(".colour")
 btn.addEventListener("click", function() {
